46d72e8c6ffe327832a51e538d8dbcee1270653c
RomanNikolaev <arem379@mail.ru>
2025-09-07 00:06:58 +0300

    init rdb in docker-compose, .env, config, rdb.go

---
+++ internal/rdb/rdb.go
@@ -0,0 +1,22 @@
+package internal
+
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+	"log/slog"
+)
+
+func NewRDB(addr string, logger *slog.Logger) redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: "",
+		DB:       0,
+	})
+
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		logger.Error("redis db init error", err)
+		return redis.Client{}
+	}
+
+	return rdb
+}
